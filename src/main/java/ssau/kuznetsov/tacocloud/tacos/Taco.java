584813fd86f976826a34273776ba91e98c62d985
Andrey <ethenaut@gmail.com>
2022-02-21 22:22:04 +0400

    impl taco

---
+++ src/main/java/ssau/kuznetsov/tacocloud/tacos/Taco.java
@@ -0,0 +1,11 @@
+package ssau.kuznetsov.tacocloud.tacos;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+    private String name;
+    private List<String> ingredients;
+}

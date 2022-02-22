d5de957d5cf80806ed4aff0649f127fcb96947f0
Andrey <ethenaut@gmail.com>
2022-02-22 18:54:08 +0400

    annotate models with JPA

--- src/main/java/ssau/kuznetsov/tacocloud/models/Taco.java
+++ src/main/java/ssau/kuznetsov/tacocloud/models/Taco.java
@@ -2,14 +2,18 @@ package ssau.kuznetsov.tacocloud.models;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Date;
 import java.util.List;
 
 @Data
+@Entity
 public class Taco {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private Date createdAt;
@@ -18,5 +22,11 @@ public class Taco {
     @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
 
+    @ManyToMany(targetEntity = Ingredient.class)
     private List<Ingredient> ingredients;
+
+    @PrePersist
+    void createdAt() {
+        this.createdAt = new Date();
+    }
 }

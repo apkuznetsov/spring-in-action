47e7441495ea4e27ff5113c9dd6366d05701d1a3
Andrey <ethenaut@gmail.com>
2022-02-22 10:39:17 +0400

    fix home controller test's imports

--- src/test/java/ssau/kuznetsov/tacocloud/HomeControllerTest.java
+++ src/test/java/ssau/kuznetsov/tacocloud/HomeControllerTest.java
@@ -1,17 +1,16 @@
 package ssau.kuznetsov.tacocloud;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import ssau.kuznetsov.tacocloud.controllers.HomeController;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(HomeController.class)

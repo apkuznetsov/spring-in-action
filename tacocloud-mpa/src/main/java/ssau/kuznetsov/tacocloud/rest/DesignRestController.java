09dcfa20fd058f849bfbbc720b882d776252b016
Andrey <ethenaut@gmail.com>
2022-02-28 12:53:11 +0400

    impl design rest controller

---
+++ tacocloud-mpa/src/main/java/ssau/kuznetsov/tacocloud/rest/DesignRestController.java
@@ -0,0 +1,33 @@
+package ssau.kuznetsov.tacocloud.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.hateoas.server.EntityLinks;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ssau.kuznetsov.tacocloud.models.Taco;
+import ssau.kuznetsov.tacocloud.repositories.TacoRepository;
+
+@RestController
+@RequestMapping(path = "/design", produces = "application/json")
+@CrossOrigin(origins = "*")
+public class DesignRestController {
+
+    private final TacoRepository tacoRepo;
+    @Autowired
+    EntityLinks entityLinks;
+
+    public DesignRestController(TacoRepository tacoRepo) {
+        this.tacoRepo = tacoRepo;
+    }
+
+    @GetMapping("/recent")
+    public Iterable<Taco> recentTacos() {
+        PageRequest page = PageRequest.of(
+                0, 12, Sort.by("createdAt").descending());
+        return tacoRepo.findAll(page).getContent();
+    }
+}

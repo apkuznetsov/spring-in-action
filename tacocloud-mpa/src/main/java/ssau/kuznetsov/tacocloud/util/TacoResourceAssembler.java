82218a854b5e61991535b5330d0388ca099c0677
Andrey <ethenaut@gmail.com>
2022-03-02 19:30:10 +0400

    fix taco resource assembler

--- tacocloud-mpa/src/main/java/ssau/kuznetsov/tacocloud/util/TacoResourceAssembler.java
+++ tacocloud-mpa/src/main/java/ssau/kuznetsov/tacocloud/util/TacoResourceAssembler.java
@@ -1,23 +1,24 @@
 package ssau.kuznetsov.tacocloud.util;
 
-import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+import org.springframework.hateoas.mvc.ResourceAssemblerSupport;
 import ssau.kuznetsov.tacocloud.models.Taco;
 import ssau.kuznetsov.tacocloud.represents.TacoResource;
 import ssau.kuznetsov.tacocloud.rest.DesignRestController;
 
 public class TacoResourceAssembler
-        extends RepresentationModelAssemblerSupport<Taco, TacoResource> {
+        extends ResourceAssemblerSupport<Taco, TacoResource> {
 
     public TacoResourceAssembler() {
         super(DesignRestController.class, TacoResource.class);
     }
 
+    @Override
     protected TacoResource instantiateResource(Taco taco) {
         return new TacoResource(taco);
     }
 
     @Override
-    public TacoResource toModel(Taco entity) {
-        return createModelWithId(entity.getId(), entity);
+    public TacoResource toResource(Taco taco) {
+        return createResourceWithId(taco.getId(), taco);
     }
 }

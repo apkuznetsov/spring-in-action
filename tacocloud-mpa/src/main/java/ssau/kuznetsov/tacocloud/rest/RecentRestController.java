585d348fb3f90861e19c643830c0f0b391c0c525
Andrey <ethenaut@gmail.com>
2022-03-03 19:35:48 +0400

    impl resource processor

--- tacocloud-mpa/src/main/java/ssau/kuznetsov/tacocloud/rest/RecentRestController.java
+++ tacocloud-mpa/src/main/java/ssau/kuznetsov/tacocloud/rest/RecentRestController.java
@@ -1,8 +1,12 @@
 package ssau.kuznetsov.tacocloud.rest;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.springframework.hateoas.Resources;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.hateoas.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import ssau.kuznetsov.tacocloud.models.Taco;
 import ssau.kuznetsov.tacocloud.repositories.TacoRepository;
@@ -14,6 +18,7 @@ import java.util.List;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
+@RepositoryRestController
 public class RecentRestController {
 
     private final TacoRepository tacoRepo;
@@ -22,21 +27,33 @@ public class RecentRestController {
         this.tacoRepo = tacoRepo;
     }
 
-    @GetMapping("/recent")
-    public Resources<TacoResource> recentTacos() {
+    @Bean
+    public ResourceProcessor<PagedResources<Resource<Taco>>>
+    tacoProcessor(EntityLinks links) {
+        return resource -> {
+            resource.add(
+                    links.linkFor(Taco.class)
+                            .slash("recent")
+                            .withRel("recents"));
+            return resource;
+        };
+    }
+
+    @GetMapping(path = "/tacos/recent", produces = "application/hal+json")
+    public ResponseEntity<Resources<TacoResource>> recentTacos() {
         PageRequest page = PageRequest.of(
                 0, 12, Sort.by("createdAt").descending());
+
         List<Taco> tacos = tacoRepo.findAll(page).getContent();
         List<TacoResource> tacoResources =
                 new TacoResourceAssembler().toResources(tacos);
 
         Resources<TacoResource> recentResources = new Resources<>(tacoResources);
         recentResources.add(
-                linkTo(methodOn(DesignRestController.class)
-                        .recentTacos())
+                linkTo(methodOn(RecentRestController.class).recentTacos())
                         .withRel("recents")
         );
 
-        return recentResources;
+        return new ResponseEntity<>(recentResources, HttpStatus.OK);
     }
 }

6ea742af3c2b0d0306290c769261ff4c07f5bee6
Andrey <ethenaut@gmail.com>
2022-07-02 09:30:43 +0400

    ingredient REST controller imports

--- tacocloud-api/src/main/java/ssau/kuznetsov/tacocloudapi/api/controllers/IngredientRestController.java
+++ tacocloud-api/src/main/java/ssau/kuznetsov/tacocloudapi/api/controllers/IngredientRestController.java
@@ -1,12 +1,17 @@
 package ssau.kuznetsov.tacocloudapi.api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import ssau.kuznetsov.tacocloud.models.Ingredient;
 import ssau.kuznetsov.tacocloud.repositories.IngredientRepository;
+import ssau.kuznetsov.tacoclouddata.data.IngredientRepository;
+import ssau.kuznetsov.tacoclouddomain.domain.Ingredient;
+
+import java.net.URI;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/ingredients", produces = "application/json")
